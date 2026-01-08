45cac954f0b50bbc50d66412c870ad4cc0d003cf
Raul Manendez <muk214782@gmail.com>
2026-01-08 07:55:08 +0530

    sliding window 1

--- src/start/slidingwindow/LongestSubArrayReplacingZeros.java
+++ src/start/slidingwindow/LongestSubArrayReplacingZeros.java
@@ -1,36 +1,56 @@
 package start.slidingwindow;
 
-
 public class LongestSubArrayReplacingZeros {
-	int longest(String s,int k) {
-		
-		int start=0, onesCount= 0, maxLength = 0;
-		for(int end=0;end<s.length();end++) {
+	int longest(String s, int k) {
+
+		int start = 0, onesCount = 0, maxLength = 0;
+		for (int end = 0; end < s.length(); end++) {
 			char endChar = s.charAt(end);
-			
-			if(endChar == '1') {
+
+			if (endChar == '1') {
 				onesCount++;
 			}
-			
-			if(end - start + 1 - onesCount > k) {
+
+			if (end - start + 1 - onesCount > k) {
 				char startChar = s.charAt(start);
-				if(startChar == '1') {
+				if (startChar == '1') {
 					onesCount--;
 				}
 				start++;
 			}
-			
+
 			maxLength = Math.max(maxLength, end - start + 1);
 		}
-		
+
 		return maxLength;
 	}
-	
+
+	int longestv2(String s, int k) {
+
+		int start = 0, maxLength = 0, zeros = 0;
+		for (int end = 0; end < s.length(); end++) {
+			if (s.charAt(end) == '0') {
+				zeros++;
+			}
+
+			while (zeros > k) {
+				if (s.charAt(start) == '0') {
+					zeros--;
+				}
+				start++;
+			}
+
+			maxLength = Math.max(maxLength, end - start + 1);
+		}
+
+		return maxLength;
+	}
+
 	public static void main(String[] args) {
-		
-		System.out.println(
-		new LongestSubArrayReplacingZeros().longest("10001010011", 2)
-				);
-		
+
+		System.out.println(new LongestSubArrayReplacingZeros().longest("10001010011", 2));
+
+		System.out.println(new LongestSubArrayReplacingZeros().longestv2("10001010011", 2));
+
 	}
 }
---
+++ src/start/slidingwindow/PermutationInAString.java
@@ -0,0 +1,66 @@
+package start.slidingwindow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PermutationInAString {
+
+	boolean has(String s, String pattern) {
+		var pMap = map(pattern);
+
+		List<Integer> result = new ArrayList<>();
+
+		int start = 0, matched = pMap.size();
+		for (int end = 0; end < s.length(); end++) {
+
+			char endChar = s.charAt(end);
+
+			if (pMap.containsKey(endChar)) {
+				pMap.put(endChar, pMap.get(endChar) - 1);
+
+				if (pMap.get(endChar) == 0)
+					matched++;
+			}
+
+			if (matched == pMap.size())
+				result.add(start);
+
+			if(start - end + 1 > pattern.length()) {
+				char startChar = s.charAt(start);
+				start++;
+				
+				if (pMap.containsKey(startChar)) {//don't do anything if end char not valid in window
+					if (pMap.get(startChar) == 0)
+						matched++;
+					
+					pMap.put(startChar, pMap.get(startChar) + 1);
+				}
+			}
+		}
+
+		System.out.println(result);
+		return !result.isEmpty();
+	}
+
+	Map<Character, Integer> map(String s) {
+		Map<Character, Integer> fm = new HashMap<>();
+		for (char c : s.toCharArray()) {
+			fm.put(c, fm.getOrDefault(c, 0) + 1);
+		}
+
+		return fm;
+	}
+
+	public static void main(String[] args) {
+		var p = new PermutationInAString();
+
+		System.out.println(p.has("oibcaf", "abc"));
+		System.out.println(p.has("odicf", "dc"));
+		System.out.println(p.has("eidbaooo", "ab"));
+		System.out.println(p.has("abcde", "abc"));
+		System.out.println(p.has("eidbaooo", "ab"));
+		System.out.println(p.has("aab", "aaa"));
+	}
+}
